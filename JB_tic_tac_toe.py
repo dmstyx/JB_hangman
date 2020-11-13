3a4a98b2217a384c4268db3e38c9a82789892466
TK <synova_matare@hotmail.co.uk>
2020-11-13 20:31:58 +0000

    added features to tic tac toe game

--- JB_tic_tac_toe.py
+++ JB_tic_tac_toe.py
@@ -10,26 +10,36 @@ print(f"""
 """)
 
 # Winning combinations
-win_lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
-count = 0
+win_lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],
+             [0, 4, 8], [6, 4, 2]]
+
+num = 0
+
 # For X to win, for Y to win and game not finished
 def win(win_lines, tics):
-  global count
-  for items in win_lines:
-      if tics[items[0]] == tics[items[1]] == tics[items[2]] == 'X':
-          count += 1
-          print("X wins")   
-      elif tics[items[0]] == tics[items[1]] == tics[items[2]] == 'O':
-          count += 1
-          print("O wins")
+    global num  
+    for items in win_lines:
+        if abs(tics.count("X") - tics.count("O")) > 1:
+            return "Impossible"
+        elif tics[items[0]] == tics[items[1]] == tics[items[2]] == 'X':
+            num += 1
+            return "X wins"
+        elif tics[items[0]] == tics[items[1]] == tics[items[2]] == 'O':
+            num += 1
+            return "O wins"
 
-win(win_lines, tics)
-print(count)
-# Draw condition
-if win(win_lines, tics) == None:
-  if count > 1 or tics.count("X") - tics.count("0"):
-      print("Impossible")
-  elif tics.count('X') + tics.count('O') < 9:
-      print("Game not finished")
-  elif tics.count("X") + tics.count("O") == 9:
-      print("Draw")
+print(win(win_lines, tics))
+print(tics.count("X") + tics.count("O"))
+print(num)
+
+if num == 0 and (tics.count("x") + tics.count("O") < 9):
+    print("Game not finished")
+    
+# # Draw condition
+# if win(win_lines, tics) == None:
+#     if count > 1 or tics.count("X") - tics.count("0"):
+#         print("Impossible")
+#     elif tics.count('X') + tics.count('O') < 9:
+#         print("Game not finished")
+#     elif tics.count("X") + tics.count("O") == 9:
+#         print("Draw")
